2c455557e755199a4df8ecfe2ca45957f2a90883
Albert Nadal Garriga <anadalg@gmail.com>
2016-12-01 00:26:32 +0100

    Integrated k-means algorithm with Kohonen algorithm.

--- kohonen/kohonen.c
+++ kohonen/kohonen.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include <math.h>
+#include <time.h>
 
+// START Kohonen Algorithm defines and global variables
 #define MAP_WIDTH 75
 #define MAP_HEIGHT 43
-#define TOTAL_SAMPLES 200
 
 typedef struct Neuron {
    unsigned int x;
@@ -37,24 +39,98 @@ Neuron** map;
 Sample* samples;
 int initial_radius = 70;
 
+// END Kohonen definitions and global variables
+
+// START k-Means Algorithm defines and global variables
+
+#define BIG_NUM          999999999999999999
+#define LINE_SIZE        80
+#define FALSE            0
+#define TRUE             1
+#define uint             unsigned int
+#define uint64           unsigned long
+#define MAX_KMEANS_ITERATIONS 100
+#define datax(i)         kmeans_data_points[i * 3]
+#define datay(i)         kmeans_data_points[i * 3 + 1]
+#define dataz(i)         kmeans_data_points[i * 3 + 2]
+#define distance(i, j)   (datax(j) - datax(i)) * (datax(j) - datax(i)) + (datay(j) - datay(i)) * (datay(j) - datay(i)) + (dataz(j) - dataz(i)) * (dataz(j) - dataz(i))
+#define is_assigned(i)   point_assignments[i].assigned
+#define set_assigned(i)  point_assignments[i].assigned = TRUE
+#define pow2(x)          ((x) * (x))
+
+typedef int bool;
+
+typedef struct {
+  uint    point_id;
+  uint    num_points;
+  uint64  sum_x;
+  uint64  sum_y;
+  uint64  sum_z;
+
+  // for calculating new centroid
+  double  target_x;
+  double  target_y;
+  double  target_z;
+  uint64  prev_distance;
+  uint64  best_distance;
+} centroid;
+
+typedef struct {
+  uint    centroid_id;
+  uint64  distance;
+  char    assigned;
+} assignment;
+
+char        *input_file_name;
+uint        total_samples;
+uint        num_clusters;
+centroid    *centroids;
+uint        *kmeans_data_points;
+assignment  *point_assignments;
+uint        improved = 0;
+float       improvement = 0.0;
+
+void setup_centroids();
+void setup_assignments();
+void update_assignments();
+void update_centroids();
+int gettimeofday(struct timeval *restrict tp, void *restrict tzp);
+
+// END k-Means definitions and global variables
+
+// START Kohonen algorithm methods
+
 unsigned int randr(unsigned int min, unsigned int max)
 {
   double scaled = (double)rand()/RAND_MAX;
   return (max - min +1)*scaled + min;
 }
 
-void initialize_samples()
+void load_and_initialize_samples()
 {
-  samples = (Sample *) malloc(sizeof(Sample) * TOTAL_SAMPLES);
+  FILE *file;
+  char line[LINE_SIZE];
 
-  for(int i=0; i<TOTAL_SAMPLES; i++) {
-    samples[i].x = randr(0,255);
-    samples[i].y = randr(0,255);
-    samples[i].z = randr(0,255);
+  // 'kmeans_data_points' is the data structure used to store the sample points for k-means algorithm process
+  kmeans_data_points = (uint *)calloc(total_samples * 3, sizeof(uint));
+
+  // 'samples' is the data structure used to store the sample points for Kohonen algorithm process
+  samples = (Sample *) malloc(sizeof(Sample) * total_samples);
+
+  // read data from file into array
+  file = fopen(input_file_name, "rt");
+  for (uint i = 0; i < total_samples; i++) {
+    fgets(line, LINE_SIZE, file);
+    sscanf(line, "%d,%d,%d", &datax(i), &datay(i), &dataz(i));
+
+    samples[i].x = datax(i);
+    samples[i].y = datay(i);
+    samples[i].z = dataz(i);
   }
+  fclose(file);
 }
 
-void initialize_map()
+void initialize_som_map()
 {
   map = (Neuron **) malloc(sizeof(Neuron *) * MAP_WIDTH);
 
@@ -75,7 +151,7 @@ void initialize_map()
 }
 
 Sample* pick_random_sample() {
-  int i = randr(0,TOTAL_SAMPLES - 1);
+  int i = randr(0,total_samples - 1);
   return &samples[i];
 }
 
@@ -196,8 +272,9 @@ char* concat(const char *s1, const char *s2)
     return result;
 }
 
-void output_html() {
+void output_html(BMU *centroids) {
 
+  bool found = FALSE;
   FILE *f = fopen("test.html", "w");
   if (f == NULL)
   {
@@ -211,7 +288,20 @@ void output_html() {
   for(int y = 0; y < MAP_HEIGHT; y++) {
     fprintf(f, "<tr>");
     for(int x = 0; x < MAP_WIDTH; x++) {
-      fprintf(f, "<td style='width:15px;height:15px;background-color:rgb(%d,%d,%d);'></td>", map[x][y].x, map[x][y].y, map[x][y].z);
+
+      found = FALSE;
+      for (int i = 0; i < num_clusters; i++) {
+        if((centroids[i].x_coord == x) && (centroids[i].y_coord == y)) {
+          found = TRUE;
+        }
+      }
+
+      if(found) {
+        fprintf(f, "<td style='width:15px;height:15px;background-color:rgb(0,0,0);'></td>");
+      } else {
+        fprintf(f, "<td style='width:15px;height:15px;background-color:rgb(%d,%d,%d);'></td>", map[x][y].x, map[x][y].y, map[x][y].z);
+      }
+
     }
     fprintf(f, "</tr>");
   }
@@ -222,16 +312,239 @@ void output_html() {
   fclose(f);
 }
 
-int main()
+// END Kohonen algorithm methods
+
+// START K-Means algorithm methods
+
+/**
+ * assign data points to current centroids
+ */
+void update_assignments()
+{
+  uint      i, j, d;
+  uint      nearest_centroid;
+  uint64    min_dist;
+  centroid  *cp, *cp_last;
+
+  // clear out centroids
+  for (cp = centroids, cp_last = centroids + num_clusters; cp < cp_last; cp++) {
+    cp->num_points = 0;
+    cp->sum_x = 0;
+    cp->sum_y = 0;
+    cp->sum_z = 0;
+  }
+
+  // for each data point...
+  for (i = 0; i < total_samples; i++) {
+    // find nearest centroid
+    for (j = 0; j < num_clusters; j++) {
+      d = distance(i, centroids[j].point_id);
+      if (j == 0 || min_dist > d) {
+        min_dist = d;
+        nearest_centroid = j;
+      }
+    }
+
+    // assign point to centroid
+    point_assignments[i].centroid_id = nearest_centroid;
+    point_assignments[i].distance = min_dist;
+
+    // update centroid stats
+    centroids[nearest_centroid].sum_x += datax(i);
+    centroids[nearest_centroid].sum_y += datay(i);
+    centroids[nearest_centroid].sum_z += dataz(i);
+    centroids[nearest_centroid].num_points++;
+  }
+
+}
+
+/**
+ * pick new centroids based on mean coordinates in the cluster
+ */
+void update_centroids()
+{
+  uint        i, a, b, c;
+  uint        *dp;
+  uint64      d;
+  float       k;
+  assignment  *ap;
+  centroid    *cp, *cp_last;
+
+  // initialize
+  for (cp = centroids, cp_last = centroids + num_clusters; cp < cp_last; cp++) {
+    cp->target_x = cp->sum_x / cp->num_points;
+    cp->target_y = cp->sum_y / cp->num_points;
+    cp->target_z = cp->sum_z / cp->num_points;
+    cp->prev_distance = cp->best_distance;
+    cp->best_distance = BIG_NUM;
+  }
+
+  // go through points; for each respective centroid, find the
+  // point closest to the target new centroid
+  for (i = 0, ap = point_assignments, dp = kmeans_data_points; i < total_samples; i++, ap++, dp += 2) {
+    // get my centroid
+    cp = centroids + ap->centroid_id;
+
+    // calculate distance to target
+    a = *dp - cp->target_x;
+    b = *(dp + 1) - cp->target_y;
+    c = *(dp + 2) - cp->target_z;
+    d = a * a + b * b + c * c;
+
+    // new closest point?
+    if (cp->best_distance > d) {
+      cp->best_distance = d;
+      cp->point_id = i;
+    }
+  }
+
+  // check improvement
+  improved = 0;
+  improvement = 0.0;
+  for (i = 0, cp = centroids; i < num_clusters; i++, cp++) {
+    if (cp->prev_distance) {
+      k = ((float)cp->best_distance - (float)cp->prev_distance) / (float)cp->prev_distance;
+      improvement += fabs(k);
+      improved++;
+    }
+  }
+
+}
+
+/**
+ * allocate the array for data point to centroid assignments
+ */
+void setup_assignments()
+{
+  point_assignments = (assignment *)calloc(total_samples, sizeof(assignment));
+}
+
+/**
+ * allocate the array for centroids, then pick the initial centroids
+ */
+void setup_centroids()
+{
+  uint i, j, k, x, y, z;
+  struct timeval tv;
+
+  // seed rand with microseconds to increase variance if called repeatedly quickly
+  gettimeofday(&tv, NULL);
+  srand(tv.tv_usec);
+
+  // allocate centroids array
+  centroids = (centroid *)calloc(num_clusters, sizeof(centroid));
+
+  // pick starting centroids, ensuring uniqueness
+  for (i = 0; i < num_clusters; ) {
+    // grab a random data point
+    j = random() % total_samples;
+
+    // take if first one or unassigned
+    if (i == 0 || !is_assigned(j)) {
+      centroids[i].point_id = j;
+
+      // mark this point (and all points like it) as assigned;
+      // this traversal works because we know the data set is sorted
+      x = datax(j);
+      y = datay(j);
+      z = dataz(j);
+      for (k = j; k < total_samples && x == datax(k) && y == datay(k) && z == dataz(k); k++)
+        set_assigned(k);
+      for (k = j; k-- > 0 && x == datax(k) && y == datay(k) && z == dataz(k); )
+        set_assigned(k);
+
+      // on to next centroid
+      i++;
+    }
+
+    // done if all assigned
+    for (j = 0; j < total_samples; j++)
+      if (!is_assigned(j)) break;
+    if (j >= total_samples) break;
+  }
+
+  // it's possible that the actual number of clusters is less than asked for
+  if (i < num_clusters)
+    num_clusters = i;
+}
+
+void search_clusters_in_samples()
+{
+  uint        i, j, x, y, z;
+  double      distance_mean, distance_sum;
+  float       average_improvement;
+  assignment  *ap, *ap_last;
+  centroid    *cp;
+
+  setup_assignments();
+  setup_centroids();
+
+  for (uint i = 0; i < MAX_KMEANS_ITERATIONS; i++) {
+    update_assignments();
+    update_centroids();
+    if (improved) {
+      average_improvement = improvement / improved;
+
+      // done if average improvement is less than 0.1%
+      if (average_improvement < 0.001)
+        break;
+    }
+  }
+
+  // calculate standard deviation of point-to-centroid distances
+  for (i = 0, cp = centroids; i < num_clusters; i++, cp++) {
+    // centroid coords
+    x = datax(cp->point_id);
+    y = datay(cp->point_id);
+    z = dataz(cp->point_id);
+
+    if (cp->num_points <= 1) {
+      printf("%d\t%d\t%d\t%d\t0.0\n", x, y, z, cp->num_points);
+    } else {
+      // calculate mean distance
+      distance_sum = 0.0;
+      for (j = 0, ap = point_assignments, ap_last = point_assignments + total_samples; ap < ap_last; j++, ap++) {
+        if (ap->centroid_id != i) continue;
+        distance_sum += sqrt(ap->distance);
+      }
+      distance_mean = distance_sum / cp->num_points;
+
+      // calculate summation for sample variance
+      distance_sum = 0.0;
+      for (ap = point_assignments, ap_last = point_assignments + total_samples; ap < ap_last; ap++) {
+        if (ap->centroid_id != i) continue;
+        distance_sum += pow2(sqrt(ap->distance) - distance_mean);
+      }
+
+      printf("%d\t%d\t%d\t%d\t%.1lf\n", x, y, z, cp->num_points, sqrt(distance_sum / (cp->num_points - 1)));
+    }
+  }
+
+}
+
+// END K-Means algorithm methods
+
+int main(int argc, char **argv)
 {
     int MAX_ITER = 1000;
     float T_INC = 1.0f/(float)(MAX_ITER);
     float t = 0.0f;
     BMU *bmu;
+    BMU *cluster_centroid_bmus;
+    BMU *centroid_bmu;
+    centroid  *cp;
+    uint i;
 
-    initialize_samples();
-    initialize_map();
-    output_html();
+    // usage: ./kohonen file_with_samples total_samples num_clusters
+    input_file_name = argv[1];
+    total_samples = atoi(argv[2]);
+    num_clusters = atoi(argv[3]);
+
+    load_and_initialize_samples();
+    search_clusters_in_samples();
+
+    initialize_som_map();
+    //output_html();
     sleep(5);
 
     int iteration_num = 0;
@@ -239,21 +552,32 @@ int main()
     while(t < 1.0f)
     {
       Sample *sample = pick_random_sample();
-//      printf("RANDOM SAMPLE X: %d | Y: %d | Z: %d\n", sample->x , sample->y, sample->z);
-
-      bmu = search_bmu(sample);
-//      printf("BMU X: %d | Y: %d | Z: %d | DIST: %f | x: %d | y: %d\n", bmu->neuron->x , bmu->neuron->y, bmu->neuron->z, bmu->distance, bmu->x_coord, bmu->y_coord);
-
+      bmu = search_bmu(sample); // Best Match Unit
       scale_neighbors(bmu, sample, t);
-
       free(bmu);
+
+      cluster_centroid_bmus = (BMU *)malloc(sizeof(BMU) * num_clusters);
+      for (i = 0, cp = centroids; i < num_clusters; i++, cp++) {
+        // centroid coords
+        Sample *centroid = (Sample *) malloc(sizeof(Sample));
+        centroid->x = datax(cp->point_id);
+        centroid->y = datay(cp->point_id);
+        centroid->z = dataz(cp->point_id);
+        centroid_bmu = (BMU *)search_bmu(centroid);
+        cluster_centroid_bmus[i].x_coord = centroid_bmu->x_coord;
+        cluster_centroid_bmus[i].y_coord = centroid_bmu->y_coord;
+        free(centroid);
+        free(centroid_bmu);
+      }
+
       t += T_INC;
 
-      output_html();
-      usleep(20000);
+      output_html(cluster_centroid_bmus);
+      free(cluster_centroid_bmus);
+
+      usleep(100000);
     }
 
-    output_html();
     free_allocated_memory();
 
     return 0;
